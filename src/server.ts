08a91a216570823452bf28a9c0fc8a6dfcb99a56
mustafaersoyer <mustafaersoyer@gmail.com>
2022-06-02 19:30:45 +0300

    use gzip

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ const swaggerUi = require("swagger-ui-express"),
 const bodyParser = require("body-parser");
 const app = express();
 import { router } from "./routes";
+const compression = require("compression");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,6 +16,8 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/api", router);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(compression());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
